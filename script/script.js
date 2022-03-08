787217a15008a850304f99ce09a756ca04bd2353
DaviMarqs <davi.amqs@hotmail.com>
2022-03-08 17:13:37 -0300

    lightbox not ready yet 2

--- script/script.js
+++ script/script.js
@@ -1,8 +1,58 @@
-const potions = document.querySelectorAll(".potion-options .row-potion1 .row-potion2");
-previewBox = document.querySelector(".preview-box");
 
-window.onload = () => {
-    for (let i = 0; i < Array.lenght; i++) {
-        
-    }
+const potions = Array.from(document.querySelectorAll('.image'));
+const previewBox = document.querySelector(".preview-box");
+
+potions.map(item => item.addEventListener('click', () => {
+    previewBox.classList.add('preview-box-active');
+}))
+
+function removePreview (){
+    previewBox.classList.remove('preview-box-active');
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('img')
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active')
+        const img = document.createElement('img')
+        img.src = image.src
+        lightbox.appendChild(img)
+    })
+})
+
+
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) return
+    lightbox.classList.remove('active')
+})
+
+*/
