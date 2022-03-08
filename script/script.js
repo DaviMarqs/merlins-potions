af7d9170b5f231f2d1f9691eb76a27d12b040ba3
DaviMarqs <davi.amqs@hotmail.com>
2022-03-07 22:45:18 -0300

    lightbox not ready yet

---
+++ script/script.js
@@ -0,0 +1,8 @@
+const potions = document.querySelectorAll(".potion-options .row-potion1 .row-potion2");
+previewBox = document.querySelector(".preview-box");
+
+window.onload = () => {
+    for (let i = 0; i < Array.lenght; i++) {
+        
+    }
+}
